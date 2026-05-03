b573d12f8f321d55fcb9ac83bf820351fe1db536
Sébastien Crozet <sebcrozet@dimforge.com>
2026-05-03 21:06:46 +0200

    feat: make inferi-shader publishable

--- crates/inferi/build.rs
+++ crates/inferi/build.rs
@@ -1,10 +1,9 @@
 use khal_builder::KhalBuilder;
 
 fn main() {
-    let shader_crate = "../inferi-shaders";
     let output_dir = "shaders-spirv";
 
-    KhalBuilder::new(shader_crate, true)
+    KhalBuilder::from_dependency("inferi-shaders", true)
         // Mandatory on the web
         .feature("unsafe_remove_boundchecks")
         .build(output_dir);
